7bb1a5079da0febf59f6753c0e5f8c4a83662650
Shnipcho <yauheni.shnip@mail.ru>
2020-04-05 21:56:37 +0300

    commit 05.04

---
+++ app/blocks/scroll/scroll.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-unused-vars */
+app.scroll = {
+  init() {
+    const scrollBtn = document.getElementById('scroll-to-top')
+    const scrollToTop = (e) => {
+      e.preventDefault();
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
+    } 
+
+    window.onscroll = () => scrollFunction()
+
+    const scrollFunction = () => {
+      if (document.documentElement.clientWidth > 991) {
+        if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+          scrollBtn.style.opacity = "1";
+        } else {
+          scrollBtn.style.opacity = "0";
+        }
+      } else {
+        if (document.body.scrollTop > 2000 || document.documentElement.scrollTop > 2000) {
+          scrollBtn.style.opacity = "1";
+        } else {
+          scrollBtn.style.opacity = "0";
+        }
+      }
+      
+    } 
+
+    scrollBtn.addEventListener('click', scrollToTop);
+  }
+}
