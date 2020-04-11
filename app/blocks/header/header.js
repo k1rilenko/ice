f3222213c26cd81af7787cf4bf7fc15385a4142e
Shnipcho <yauheni.shnip@mail.ru>
2020-04-11 11:31:09 +0300

    11.04 #2

--- app/blocks/header/header.js
+++ app/blocks/header/header.js
@@ -5,7 +5,7 @@ app.header = {
 	},
 	changePosition() {
 		window.addEventListener('scroll', () => {
-			if (header && document.documentElement.scrollTop > 80) {
+			if (app.header.header && document.documentElement.scrollTop > 80) {
 				this.header.style = 'background-color: #ffffff;';
 			} else {
 				this.header.style = '';
